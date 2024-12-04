3238e7d497b6deec0abfa0cda844115bdff2567c
Lakshmikanth <kanthreddy183@gmail.com>
2024-12-04 15:02:28 +0530

    delete variables

--- src/main/java/com/kanth/Ecomm_Project/EcommProjectApplication.java
+++ src/main/java/com/kanth/Ecomm_Project/EcommProjectApplication.java
@@ -7,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EcommProjectApplication {
 
 	public static void main(String[] args) {
-		int i=10;
-		int j=20;
+		
 		SpringApplication.run(EcommProjectApplication.class, args);
 	}
 
