cdada493a691e71d99c2e81575bcee818b53c65f
Lakshmikanth <kanthreddy183@gmail.com>
2024-12-04 14:43:05 +0530

    first commit

---
+++ src/main/java/com/kanth/Ecomm_Project/EcommProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kanth.Ecomm_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommProjectApplication.class, args);
+	}
+
+}
