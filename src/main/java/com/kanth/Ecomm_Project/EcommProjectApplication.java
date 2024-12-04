7c01e71e75c67b0074dbf9e4b737c2a470d61575
Lakshmikanth <kanthreddy183@gmail.com>
2024-12-04 14:58:56 +0530

    added variables

--- src/main/java/com/kanth/Ecomm_Project/EcommProjectApplication.java
+++ src/main/java/com/kanth/Ecomm_Project/EcommProjectApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EcommProjectApplication {
 
 	public static void main(String[] args) {
+		int i=10;
+		int j=20;
 		SpringApplication.run(EcommProjectApplication.class, args);
 	}
 
